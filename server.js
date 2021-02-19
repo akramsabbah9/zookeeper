358b7afada530ccbeb3a76295f50ba908d7237d6
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-18 21:29:21 -0800

    modularized code

--- server.js
+++ server.js
@@ -1,132 +1,20 @@
-const fs = require("fs");
-const path = require("path");
 const express = require("express");
-const { animals } = require("./data/animals");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+
 // parse incoming JSON data
 app.use(express.json());
 
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
 app.use(express.static('public'));
-
-
-const filterByQuery = function(query, animalsArray) {
-    let personalityTraitsArray = [];
-    let filteredResults = animalsArray;
-
-    if (query.personalityTraits) {
-        // save personalityTraits as a dedicated array
-        // if it's a string, place into new array and save
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
-        }
-        else {
-            personalityTraitsArray = query.personalityTraits;
-        }
-        personalityTraitsArray.forEach(trait => {
-            // check the trait against each animal in the filteredResults array
-            filteredResults = filteredResults.filter(
-                animal => animal.personalityTraits.indexOf(trait) !== -1
-            );
-        });
-    }
-
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;
-};
-
-const findById = function(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-};
-
-// add animal to animals.json and animals array
-function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-    fs.writeFileSync(
-        path.join(__dirname, "./data/animals.json"),
-        JSON.stringify({ animals: animalsArray }, null, 2)
-    );
-
-    // return finished data to the POST route for response
-    return animal;
-}
-
-const validateAnimal = function(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-        return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-        return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-        return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-        return false;
-    }
-    return true;
-};
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/animals", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/animals.html"));
-});
-
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-/* Query routes */
-app.get("/api/animals", (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-/* param routes */
-app.get("/api/animals/:id", (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    }
-    else {
-        res.sendStatus(404);
-    }
-});
-
-/* Post requests */
-app.post("/api/animals", (req, res) => {
-    // set id to next possible index
-    req.body.id = animals.length.toString();
-
-    // if any data in req.body is incorrect, send 400 error
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
-    }
-    else {
-        const animal = createNewAnimal(req.body, animals);
-        res.json(animal);
-    }
-});
 
 
 app.listen(PORT, () => {
